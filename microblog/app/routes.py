13079da128f7345102fe90a0e98cdd090bb99893
liusheen <candlexg@gmail.com>
2020-04-15 00:40:03 -0400

    add user home profile info

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -21,7 +21,6 @@ def index():
     ]
     return render_template('index.html', title='Home Page', posts=posts)
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -57,3 +56,13 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
